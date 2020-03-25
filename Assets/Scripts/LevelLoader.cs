474f85535c43317314b7a9f7f88cf7a06dfb54eb
loChris <44450518+loChris@users.noreply.github.com>
2020-03-25 16:33:42 -0500

    added a sound to the loading splash screen and music to the main menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private int _currentSceneIndex;
+    [SerializeField] private float _waitForSeconds = 3f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        
+        if (_currentSceneIndex == 0)
+            StartCoroutine(LoadSceneRoutine());
+    }
+
+    IEnumerator LoadSceneRoutine()
+    {
+        yield return new WaitForSeconds(_waitForSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(_currentSceneIndex + 1);
+    }
+}
