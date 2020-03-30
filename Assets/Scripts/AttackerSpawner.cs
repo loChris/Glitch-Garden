c39bfb0785cae73c69cd880eb68b767ba587ba07
loChris <44450518+loChris@users.noreply.github.com>
2020-03-30 13:48:16 -0500

    created a basic enemy spawner (wip)

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] private bool _spawn = true;
+    [SerializeField] private GameObject _lizardMan;
+    
+    void Start()
+    {
+        StartCoroutine(EnemySpawnerRoutine());
+    }
+
+    IEnumerator EnemySpawnerRoutine()
+    {
+        while (_spawn)
+        {
+            Instantiate(_lizardMan);
+            yield return new WaitForSeconds(Random.Range(1f, 5f));
+        }
+    }
+}
