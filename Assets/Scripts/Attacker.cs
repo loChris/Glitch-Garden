8ff940fc33b4581914431bc119a27b67e862d21a
loChris <44450518+loChris@users.noreply.github.com>
2020-03-30 12:45:49 -0500

    lizard enemy now moves

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)] [SerializeField] private float _speed = .5f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * (_speed * Time.deltaTime));
+    }
+}
