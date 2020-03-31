00d4b38b13a881062918bd2fe2b74460930c740c
loChris <44450518+loChris@users.noreply.github.com>
2020-03-31 12:54:09 -0500

    added a projectile for the cactus defender to shoot

---
+++ Assets/Scripts/DefenderShoot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderShoot : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _zucchiniGameObject;
+    [SerializeField] private GameObject _gunGameObject;
+    
+    public void Fire()
+    {
+        Instantiate(_zucchiniGameObject, _gunGameObject.transform.position, transform.rotation);
+    }
+}
