3eab095dfad8a6b52c0fdcbf295d57465b700868
loChris <44450518+loChris@users.noreply.github.com>
2020-03-31 13:06:07 -0500

    added projectile movement to the zucchini which fires out of the cactus

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2f;
+    
+    private void Update()
+    {
+        transform.Translate(Vector2.right * (_speed * Time.deltaTime));
+
+    }
+}
