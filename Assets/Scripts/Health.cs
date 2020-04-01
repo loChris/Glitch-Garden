b322c79cf47071cbe18c27faa0abee4f0bb5a262
loChris <44450518+loChris@users.noreply.github.com>
2020-03-31 19:34:24 -0500

    added a health system for the lizards and a damage system for projectiles - I also added spawners for the multiple lanes

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _health = 100f;
+
+    public void DealDamage(float damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _damage = 50f;
     
     private void Update()
     {
         transform.Translate(Vector2.right * (_speed * Time.deltaTime));
+    }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var health = other.GetComponent<Health>();
+        
+        if (health != null)
+            health.DealDamage(_damage);
     }
 }
