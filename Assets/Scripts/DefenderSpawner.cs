d5dd3afcd377281e0521266c6054556981799514
loChris <44450518+loChris@users.noreply.github.com>
2020-04-25 13:00:31 -0400

    mouse click will spawn defenders (still in the 0 0 pos)

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject defender;
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnDefender();
+        }    
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(
+            defender,
+            transform.position,
+            quaternion.identity
+        ) as GameObject;
+    }
+}
