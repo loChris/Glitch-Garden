0715bd3612a40594fe304e4cb75580b96c44e38b
loChris <44450518+loChris@users.noreply.github.com>
2020-05-07 15:50:34 -0400

    added an animation for star resource generation

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] private int starPrice = 100;
+
+    public void AddStars(int amount)
+    {
+        FindObjectOfType<StarDisplay>().AddStars(amount);
+    }
 }
