0b1315864e7697e3c6eee5dd9cde8c685c244ec1
loChris <44450518+loChris@users.noreply.github.com>
2020-05-07 16:38:50 -0400

    working on code to stop or start shooting animation if attacker is in defender lane

--- Assets/Scripts/DefenderShoot.cs
+++ Assets/Scripts/DefenderShoot.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class DefenderShoot : MonoBehaviour
@@ -7,6 +9,52 @@ public class DefenderShoot : MonoBehaviour
 
     [SerializeField] private GameObject _zucchiniGameObject;
     [SerializeField] private GameObject _gunGameObject;
+    private AttackerSpawner myLaneSpawner;
+
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (AttackerInLane())
+        {
+            Debug.Log("shoot");
+            //change animation state to shooting
+        }
+        else
+        {
+            Debug.Log("idle");
+            //change animation state to idle
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner attackerSpawner in attackerSpawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(attackerSpawner.transform.position.y - transform.position.y) - 0.5 <= Mathf.Epsilon);
+
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = attackerSpawner;
+            }
+        }
+    }
+
+    private bool AttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
     
     public void Fire()
     {
