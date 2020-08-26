731cb58d72f4a518fba324166b021c700cca60a6
loChris <44450518+loChris@users.noreply.github.com>
2020-08-26 18:33:57 -0400

    added a fox enenmy with animations (still doesn't dynamically spawn in)

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject otherObject = other.gameObject;
+        
+        if(otherObject.GetComponent<Gravestone>())
+        {
+            _animator.SetTrigger("JumpTrigger");
+        }
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}
